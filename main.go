4466f8c10b7b856e3360eb0593e906745f7b9cb6
James Walter <james.walter@grainger.com>
2018-03-08 15:19:12 -0600

    added hostname, visit counter, and born on

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
+var count int
+var born time.Time
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+
+	myname, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "Hello from container %q\n", myname)
+	count++
+	fmt.Fprintf(w, "I have been visted %d times\n", count)
+	fmt.Fprintf(w, "I was born %q \n", born.Format(time.UnixDate))
 }
 
 func main() {
+	born = time.Now()
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
